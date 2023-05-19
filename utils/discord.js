cbf88aebdab56917e114a6382218e95458a74112
renny <renny.vanneste@hootsuite.com>
2023-05-19 14:58:21 -0600

    bit of logging for messages

--- utils/discord.js
+++ utils/discord.js
@@ -1,13 +1,25 @@
 const messages = {};
 
 const getMessage = (orgId, messageId) => {
-	console.log(`getDiscordMessages: ${orgId}, ${messageId}`);
+	console.log(`getMessage: ${orgId}, ${messageId}`);
 	return messages[messageId];
 };
 
-const addMessage = (orgId, messageId, data) => messages[messageId] = data;
-const hasMesssage = (orgId, messageId) => messageId in messages;
-const removeMessage = (orgId, messageId) => delete messages[messageId];
+const addMessage = (orgId, messageId, data) => {
+	console.log(`addMessage: ${orgId}, ${messageId}`, data);
+	messages[messageId] = data;
+	console.log('current messages:', messages);
+};
+
+const hasMesssage = (orgId, messageId) => {
+	console.log(`hasMesssage: ${orgId}, ${messageId}`);
+	return messageId in messages;
+};
+
+const removeMessage = (orgId, messageId) => {
+	console.log(`removeMessage: ${orgId}, ${messageId}`);
+	delete messages[messageId];
+};
 
 module.exports = {
 	getMessage,
