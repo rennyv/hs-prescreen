17c6296f01833f00f29f8b5c73cf02c743f051c6
renny <renny.vanneste@hootsuite.com>
2023-05-23 13:03:24 -0600

    better error messages for discord messaging

--- utils/discord.js
+++ utils/discord.js
@@ -2,23 +2,23 @@ const messages = {};
 
 const getMessage = (orgId, messageId) => {
 	console.log(`getMessage: ${orgId}, ${messageId}`);
-	return messages[messageId];
+	return messages[`${orgId},${messageId}`];
 };
 
 const addMessage = (orgId, messageId, data) => {
 	console.log(`addMessage: ${orgId}, ${messageId}`, data);
-	messages[messageId] = data;
+	messages[`${orgId},${messageId}`] = data;
 	console.log('current messages:', messages);
 };
 
 const hasMesssage = (orgId, messageId) => {
 	console.log(`hasMesssage: ${orgId}, ${messageId}`);
-	return messageId in messages;
+	return (`${orgId},${messageId}`) in messages;
 };
 
 const removeMessage = (orgId, messageId) => {
 	console.log(`removeMessage: ${orgId}, ${messageId}`);
-	delete messages[messageId];
+	delete messages[`${orgId},${messageId}`];
 };
 
 module.exports = {
